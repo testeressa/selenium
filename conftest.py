6f607723a909543902059ba9f157f63bfa84ebc5
maria.kosheleva <maria.kosheleva@corp.mail.ru>
2025-03-08 14:40:36 +0300

    добавила возможность удалённого запуска тестов

--- conftest.py
+++ conftest.py
@@ -3,19 +3,24 @@ import logging
 import allure
 import datetime
 import pytest
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FFOptions
 from selenium.webdriver.safari.options import Options as SFOptions
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", default="Chrome")
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--browser_version", default="128.0")
     parser.addoption("--headless", action="store_true")
     parser.addoption("--url", default="http://192.168.0.23:8081")
     parser.addoption("--log_level", action="store", default="INFO")
+    parser.addoption(
+        "--selenoid", action="store_true", help="Use Selenoid for remote browser execution"
+    )
+    parser.addoption(
+        "--selenoid_url", default="http://localhost:4444/wd/hub", help="Selenoid hub URL"
+    )
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
@@ -27,10 +32,13 @@ def pytest_runtest_makereport(item, call):
 
 @pytest.fixture()
 def browser(request):
-    browser_name = request.config.getoption("browser")
+    browser_name = request.config.getoption("browser").lower()
+    browser_version = request.config.getoption("browser_version")
     headless = request.config.getoption("headless")
     url = request.config.getoption("url")
-    log_level = request.config.getoption("--log_level")
+    log_level = request.config.getoption("log_level")
+    use_selenoid = request.config.getoption("selenoid")
+    selenoid_url = request.config.getoption("selenoid_url")
 
     logs_dir = "logs"
     if not os.path.exists(logs_dir):
@@ -44,31 +52,80 @@ def browser(request):
 
     logger.info("===> Test started at %s" % datetime.datetime.now())
 
-    if browser_name in ["Chrome", "ch"]:
-        options = ChromeOptions()
-        if headless:
-            options.add_argument("--headless=new")
-        driver = webdriver.Chrome(options=options)
-    elif browser_name in ["Firefox", "ff"]:
-        options = FFOptions()
-        if headless:
-            options.add_argument("--headless")
-        driver = webdriver.Firefox(options=options)
-    elif browser_name in ["Safari", "sf"]:
-        options = SFOptions()
-        if headless:
-            options.add_argument("--headless")
-        driver = webdriver.Safari(options=options)
-    elif browser_name in ["Yandex", "ya"]:
-        service = Service(executable_path="/Users/maria.kosheleva/Downloads/drivers/yandexdriver")
-        options = ChromeOptions()
-        if headless:
-            options.add_argument("--headless=new")
-        options.binary_location = "/Applications/Yandex.app"
-        driver = webdriver.Chrome(service=service, options=options)
+    if use_selenoid:
+        if browser_name == "chrome":
+            options = ChromeOptions()
+            options.set_capability("browserName", "chrome")
+            options.set_capability("browserVersion", browser_version)
+            options.set_capability("selenoid:options", {
+                "enableVNC": True,
+                "enableVideo": False,
+                "enableLog": True,
+            })
+            if headless:
+                options.add_argument("--headless=new")
+        elif browser_name == "firefox":
+            options = FFOptions()
+            options.set_capability("browserName", "firefox")
+            options.set_capability("browserVersion", browser_version)
+            options.set_capability("selenoid:options", {
+                "enableVNC": True,
+                "enableVideo": False,
+                "enableLog": True,
+            })
+            if headless:
+                options.add_argument("--headless")
+        elif browser_name == "safari":
+            options = SFOptions()
+            options.set_capability("browserName", "safari")
+            options.set_capability("browserVersion", browser_version)
+            options.set_capability("selenoid:options", {
+                "enableVNC": True,
+                "enableVideo": False,
+                "enableLog": True,
+            })
+            if headless:
+                options.add_argument("--headless")
+        elif browser_name == "yandex":
+            options = ChromeOptions()
+            options.set_capability("browserName", "chrome")  # Yandex использует движок Chrome
+            options.set_capability("browserVersion", browser_version)
+            options.set_capability("selenoid:options", {
+                "enableVNC": True,
+                "enableVideo": False,
+                "enableLog": True,
+            })
+            if headless:
+                options.add_argument("--headless=new")
+
+        driver = webdriver.Remote(
+            command_executor=selenoid_url,
+            options=options
+        )
+    else:
+        if browser_name == "chrome":
+            options = ChromeOptions()
+            if headless:
+                options.add_argument("--headless=new")
+            driver = webdriver.Chrome(options=options)
+        elif browser_name == "firefox":
+            options = FFOptions()
+            if headless:
+                options.add_argument("--headless")
+            driver = webdriver.Firefox(options=options)
+        elif browser_name == "safari":
+            options = SFOptions()
+            if headless:
+                options.add_argument("--headless")
+            driver = webdriver.Safari(options=options)
+        elif browser_name == "yandex":
+            options = ChromeOptions()
+            if headless:
+                options.add_argument("--headless=new")
+            options.binary_location = "/Applications/Yandex.app"
+            driver = webdriver.Chrome(options=options)
 
     driver.maximize_window()
-
     driver.get(url)
     driver.url = url
 
